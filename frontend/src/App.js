2214bf78a1e18edec1f2620fbf8933913bbd4da1
Kasperone <dwaka87@o2.pl>
2022-04-29 22:51:10 +0200

    bootstrap-added_header-and-footer_components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
+import Header from "./components/Header"
+import { Container } from "react-bootstrap"
+import Footer from "./components/Footer"
+
 const App = () => {
 	return (
 		<>
-			<h1>Welcome To Shop</h1>
+			<Header />
+			<main className='py-3'>
+				<Container>
+					<h1>Welcome To Shop</h1>
+				</Container>
+			</main>
+			<Footer />
 		</>
 	)
 }
