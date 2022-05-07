ab7174ec7134e2662ec5216e2397bcb62133b778
Kasperone <dwaka87@o2.pl>
2022-05-07 16:39:04 +0200

    added_cart_functionality

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,4 +1,3 @@
-import { Action } from "history"
 import { CART_ADD_ITEM } from "../constants/cartConstants"
 
 export const cartReducer = (state = { cartItems: [] }, action) => {
--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,6 +1,31 @@
-import React from "react"
+import React, { useEffect } from "react"
+import { Link, useParams, useNavigate, useLocation } from "react-router-dom"
+import { useDispatch, useSelector } from "react-redux"
+import { Row, Col, ListGroup, Image, Form, Button, Cart } from "react-bootstrap"
+import Message from "../components/Message"
+import { addToCart } from "../actions/cartActions"
 
 const CartScreen = () => {
+	const { id } = useParams()
+	// const navigate = useNavigate()
+	const location = useLocation()
+	const productId = id
+	const qty = new URLSearchParams(location.search).get("qty")
+	// const qty = location.search ? Number(location.search.split("=")[1]) : 1
+
+	const dispatch = useDispatch()
+
+	const cart = useSelector(state => state.cart)
+	const { cartItems } = cart
+
+	console.log(cartItems)
+
+	useEffect(() => {
+		if (productId) {
+			dispatch(addToCart(productId, qty))
+		}
+	}, [dispatch, productId, qty])
+
 	return <div>CartScreen</div>
 }
 
