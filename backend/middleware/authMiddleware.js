4e5bea4d28e3cd4bc1b6d37a069ceee267759478
Kasperone <dwaka87@o2.pl>
2022-05-09 21:41:22 +0200

    custom_authe_middleware_done

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,35 @@
+import jwt from "jsonwebtoken"
+import asyncHandler from "express-async-handler"
+import User from "../models/userModel.js"
+
+const protect = asyncHandler(async (req, res, next) => {
+	let token
+	console.log(req.headers.authorization)
+	if (
+		req.headers.authorization &&
+		req.headers.authorization.startsWith("Bearer")
+	) {
+		try {
+			token = req.headers.authorization.split(" ")[1]
+			console.log(req.headers.authorization)
+			console.log(token)
+
+			const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+			req.user = await User.findById(decoded.id).select("-password")
+
+			next()
+		} catch (error) {
+			console.error(error)
+			res.status(401)
+			throw new Error("Not authoraized, token failed")
+		}
+	}
+
+	if (!token) {
+		res.status(401)
+		throw new Error("Not authorized token")
+	}
+})
+
+export { protect }
